d867554cd38796c3da507b9e1177fff3d476206d
goni715 <halesgoni715149123@gmail.com>
2025-02-22 08:07:07 +0600

    login protected-account

--- src/app/controllers/ProtectedController.ts
+++ src/app/controllers/ProtectedController.ts
@@ -4,33 +4,36 @@ import ProtectFileOrFolderService from "../services/protected/ProtectFileOrFolde
 import UnprotectFileOrFolderService from "../services/protected/UnprotectFileOrFolderService";
 import LoginProtectedAccountService from "../services/protected/LoginProtectedAccountService";
 
-
 const createProtectedAccount = async (req: Request, res: Response) => {
-    const loginUserId = req.headers.id;
-    const { password } = req.body;
-    await CreateProtectedAccountService(res, password, loginUserId as string);
+  const loginUserId = req.headers.id;
+  const { password } = req.body;
+  await CreateProtectedAccountService(res, password, loginUserId as string);
 };
 
 const loginProtectedAccount = async (req: Request, res: Response) => {
-    const fileOrFolderId = req.params.fileOrFolderId;
-    const loginUserId = req.headers.id;
-    await LoginProtectedAccountService(res, fileOrFolderId, loginUserId as string);
+  const { password } = req.body;
+  const loginUserId = req.headers.id;
+  await LoginProtectedAccountService(
+    res,
+    password,
+    loginUserId as string
+  );
 };
 
 const protectFileOrFolder = async (req: Request, res: Response) => {
-    const fileOrFolderId = req.params.fileOrFolderId;
-    const loginUserId = req.headers.id;
-    await ProtectFileOrFolderService(res, fileOrFolderId, loginUserId as string);
+  const fileOrFolderId = req.params.fileOrFolderId;
+  const loginUserId = req.headers.id;
+  await ProtectFileOrFolderService(res, fileOrFolderId, loginUserId as string);
 };
 
 const unprotectFileOrFolder = async (req: Request, res: Response) => {
-    const fileOrFolderId = req.params.fileOrFolderId;
-    const loginUserId = req.headers.id;
-    await UnprotectFileOrFolderService(res, fileOrFolderId, loginUserId as string);
+  const fileOrFolderId = req.params.fileOrFolderId;
+  const loginUserId = req.headers.id;
+  await UnprotectFileOrFolderService(
+    res,
+    fileOrFolderId,
+    loginUserId as string
+  );
 };
 
-export {
-    createProtectedAccount,
-    protectFileOrFolder,
-    unprotectFileOrFolder
-}
+export { createProtectedAccount, loginProtectedAccount, protectFileOrFolder, unprotectFileOrFolder };
